e042355c0ee22eabbf8263f97b8426aaa4d4d453
Shen Zheng <shenzheng@google.com>
2025-07-03 18:12:09 +0000

    7_3_v1 split run.sh into train/test.sh

---
+++ test.sh
@@ -0,0 +1,8 @@
+# => test
+# bash ./tools/dist_test_map.sh \
+#     ./projects/configs/hrmapnet/hrmapnet_maptrv2_av2_r50_30ep.py \
+#     ./pretrained/hrmapnet_maptrv2_av2_ep30.pth 4
+
+# bash ./tools/dist_test_map.sh \
+#     ./projects/configs/hrmapnet/hrmapnet_maptrv2_av2_r50_30ep_geosplit.py \
+#     ./pretrained/hrmapnet_maptrv2_av2_ep30.pth 4
